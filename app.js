99a828c6de598b5eeda82752ad956f6f3e6d7ff8
Mohit Kumar <27889519+CodeCramp@users.noreply.github.com>
2018-08-31 17:39:48 +0000

    Add basic campgrounds page with campground images

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+          {name: "Salmon Creek", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
+          {name: "Granite Hill", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
+          {name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8671/16642502436_e6d611bcb5.jpg"}
+        ]
+    res.render("campgrounds", {campgrounds: campgrounds});    
+});
+
 
 // Server setting specific to Cloud9, change once hosted on personal server.
 app.listen(process.env.PORT, process.env.IP, function(){
