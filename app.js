4243814d5a6da0b87df8cd464f99ac00b2b6aee7
Mohit Kumar <27889519+CodeCramp@users.noreply.github.com>
2018-09-01 08:42:14 +0000

    Setup mongoDb database and fill in some dummy entries to get started

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 // so that we don't have to write the extension .ejs when providing ejs file-names.
 app.set("view engine", "ejs");
 
+// Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
 
-var campgrounds = [
-          {name: "Salmon Creek", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
-          {name: "Granite Hill", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
-          {name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8671/16642502436_e6d611bcb5.jpg"},
-          {name: "Salmon Creek", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
-          {name: "Granite Hill", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
-          {name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8671/16642502436_e6d611bcb5.jpg"},
-          {name: "Salmon Creek", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
-          {name: "Granite Hill", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
-          {name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8671/16642502436_e6d611bcb5.jpg"},
-          {name: "Salmon Creek", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
-          {name: "Granite Hill", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"},
-          {name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8671/16642502436_e6d611bcb5.jpg"}
-        ]
+var Campground = mongoose.model("Campground", campgroundSchema);
 
+// Campground.create({
+//     name: "Granite Hill",
+//     image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f5c67bafefbcbc_340.jpg"
+// }, function(err, campground){
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("Newly Created Campground:");
+//         console.log(campground);
+//     }
+// });
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});    
+    //Get all the campgrounds from DB
+    Campground.find({}, function(err, campgrounds){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: campgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
+    // campgrounds.push(newCampground);
     res.redirect("/campgrounds");
 });
 
