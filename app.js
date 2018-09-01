562bbf034451a6d74430ca8dddec10c05413e6d1
Mohit Kumar <27889519+CodeCramp@users.noreply.github.com>
2018-09-01 22:30:21 +0000

    Setsup separate DBs for local and production
    1. localhost will be used locally and exported in DATABASEURL
    2. Mongo Cloud DB will be used on heroku and DATABASEURL env var is updated at heroku accordingly

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var commentRoutes       = require("./routes/comments"),
     authRoutes          = require("./routes/auth");
 
     
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
