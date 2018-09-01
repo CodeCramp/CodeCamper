cb1eb896bb641e7f6fb1bd345edbf77e5272f876
Mohit Kumar <27889519+CodeCramp@users.noreply.github.com>
2018-09-01 17:27:37 +0000

    Add support for adding comments to the campgrounds

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get("/campgrounds", function(req, res){
         if(err) {
             console.log(err);
         } else {
-            res.render("index", {campgrounds: campgrounds});
+            res.render("campgrounds/index", {campgrounds: campgrounds});
         }
     });
 });
@@ -44,7 +44,7 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/new", function(req, res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 // SHOW - Show more info about a specific campground
@@ -55,11 +55,44 @@ app.get("/campgrounds/:id", function(req, res){
             console.log(err);
         } else {
             // Render the found campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
-    
 });
+
+
+// ====================
+// COMMENTS ROUTES
+// ====================
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+         if(err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+         if(err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+             });
+        }
+    });
+});
+
 
 
 // Server setting specific to Cloud9, change once hosted on personal server.
